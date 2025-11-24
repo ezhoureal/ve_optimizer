0cc5f16556e842f978676814a96c09ec3290a85c
Tianer Zhou <ezhoureal@gmail.com>
2025-11-24 13:50:25 +0800

    fix perf script
    Signed-off-by: Tianer Zhou <ezhoureal@gmail.com>

--- sp_perf.sh
+++ sp_perf.sh
@@ -5,9 +5,10 @@ timestamp=$(date +%H%M%S)
 filename="sp_${timestamp}.csv"
 
 hdc shell rm /data/local/tmp/data.csv
-hdc shell "SP_daemon -N 5 -g -ci -r"
+hdc shell "SP_daemon -N 4 -g -ci -r" &
+p1=$!
 hdc shell aa start -b com.example.glass -a EntryAbility
-sleep 3
+wait $p1
 
 hdc file recv /data/local/tmp/data.csv "$filename"
 hdc shell aa force-stop com.example.glass
